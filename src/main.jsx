2d6be57a485cc158bd7a34103c3b88abca82001e
gabywakil <149974228+gabywakil@users.noreply.github.com>
2026-02-19 00:26:51 -0500

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/globals.css';
 
+import { HashRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </HashRouter>
+  </React.StrictMode>
 );
